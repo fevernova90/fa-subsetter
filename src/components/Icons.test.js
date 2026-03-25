bf105681cef50465dc6c85a035f14e6139c7516e
Muhaimin Taib <muhaimin@gotelz.com>
2026-03-25 15:50:32 +0800

    test(01-01): add component render tests for Icons, AddIconForm, Generator
    - Icons.test.js: render test with thunk middleware store + fetch mock
    - AddIconForm.test.js: render test with Redux Provider
    - Generator.test.js: render test with Redux Provider
    - Auto-fixed: added applyMiddleware(thunk) to Icons test store (thunk dispatch)

---
+++ src/components/Icons.test.js
@@ -0,0 +1,30 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux';
+import { createStore, applyMiddleware } from 'redux';
+import thunk from 'redux-thunk';
+import rootReducer from '../reducers';
+import Icons from './Icons';
+
+const store = createStore(rootReducer, applyMiddleware(thunk));
+
+beforeEach(() => {
+  global.fetch = jest.fn(() =>
+    Promise.resolve({ json: () => Promise.resolve([]) })
+  );
+});
+
+afterEach(() => {
+  global.fetch.mockRestore && global.fetch.mockRestore();
+});
+
+it('renders without crashing', () => {
+  const div = document.createElement('div');
+  ReactDOM.render(
+    <Provider store={store}>
+      <Icons />
+    </Provider>,
+    div
+  );
+  ReactDOM.unmountComponentAtNode(div);
+});
